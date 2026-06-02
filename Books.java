b993a435afd526aa90b9679c56df7845bb6723b1
Aniket Singh <asaniket.singh422@gmail.com>
2026-06-02 21:54:45 +0530

    Day1 building book class

--- Books.java
+++ Books.java
@@ -5,4 +5,16 @@ public class Books {
     String genre;
     int totalCopies;
     int availableCopies;
+
+
+    //constructor which is default when there is not more information we will use it then
+
+    Books(){
+        this.id=0;
+        this.name="Unknown name";
+        this.author="Unknown author";
+        this.genre="Unknown genre";
+        this.totalCopies=0;
+        this.availableCopies=0;
+    }
 }
