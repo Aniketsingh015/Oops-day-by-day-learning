a4d632f7a75683ac9e3cea7fb3019b131ffddf69
Aniket Singh <asaniket.singh422@gmail.com>
2026-06-10 21:59:35 +0530

    Day2 encapsulation changes in member file

--- Member.java
+++ Member.java
@@ -18,5 +18,36 @@
 import java.util.ArrayList;
 
 public class Member {
+ 
+    // =========================================================
+    // FIELDS — all private
+    // =========================================================
+ 
+    private int id;
+    private String name;
+    private String email;
+    private String memberType;          // "STUDENT" or "TEACHER"
+    private int maxBorrowLimit;         // students=3, teachers=5
+ 
+    // ArrayList<String> means: a list that holds String values
+    // Each String will be a book title the member has borrowed
+    // We'll upgrade this to List<Book> on Day 6
+    private ArrayList<String> borrowedBooks;
+ 
+ 
+    // =========================================================
+    // CONSTRUCTOR — Parameterized
+    // =========================================================
+    // We set maxBorrowLimit automatically based on memberType
+    // so the caller doesn't have to worry about the rules
+ 
+    public Member(int id, String name, String email, String memberType) {
+        setId(id);
+        setName(name);
+        setEmail(email);
+        setMemberType(memberType);     // this also sets maxBorrowLimit
+        this.borrowedBooks = new ArrayList<>();  // start with empty list
+    }
+    
     
 }
